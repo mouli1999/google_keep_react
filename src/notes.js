f8ad7253c5147276988a41295d36a91c8c909d6f
Your Name <you@example.com>
2023-03-19 00:05:01 +0200

    basic add note feature in react

---
+++ src/notes.js
@@ -0,0 +1,26 @@
+// const notes = [
+//   {
+//     key: 1,
+//     title: "Delegation",
+//     content:
+//       "Q. How many programmers does it take to change a light bulb? A. None-it's a hardware problem "
+//   },
+//   {
+//     key: 2,
+//     title: "Loops",
+//     content:
+//       "Q. How to keep a programmer in shower forever? A. Show him the shampoo bottol instruction: Lather. Rinse. Repeat."
+//   },
+//   {
+//     key: 3,
+//     title: "Arrays",
+//     content:
+//       "Q.Why did the programmer quit his job? A. Because he did not get arrays"
+//   },
+//   {
+//     key: 4,
+//     title: "Big ideas",
+//     content: "Eat more sushi"
+//   }
+// ];
+// export default notes;
